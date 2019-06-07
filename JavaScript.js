c066703256b0a9439747c93c0f6a83de23e677ad
tonylomax <antl.lomax@gmail.com>
2019-06-07 21:43:21 +0100

    Start creating JS code

---
+++ JavaScript.js
@@ -0,0 +1,27 @@
+ let currentInput = document.getElementById("input")
+
+console.log(currentInput.textContent)
+
+function sum(a,b){
+    
+}
+
+
+
+var Num1 = document.getElementById('Num1');
+var Num2 = document.getElementById('Num2');
+var Num3 = document.getElementById('Num3');
+var Num4 = document.getElementById('Num4');
+var Num5 = document.getElementById('Num5');
+var Num6 = document.getElementById('Num6');
+var Num7 = document.getElementById('Num7');
+var Num8 = document.getElementById('Num8');
+var Num9 = document.getElementById('Num9');
+
+
+Num1.addEventListener('click', function() {currentInput.textContent = 1;}, false);
+Num2.addEventListener('click', function() {currentInput.textContent = 2;}, false);
+
+function wellNamedFunction() {
+
+}
